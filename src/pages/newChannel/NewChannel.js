22acc8e9018c9aafb87f87a6ebd2045e23c1676b
soukainahikma <soukainahikma@gmail.com>
2022-05-14 17:53:23 +0100

    working on channel creation

--- src/pages/newChannel/NewChannel.js
+++ src/pages/newChannel/NewChannel.js
@@ -1,37 +1,37 @@
 import classes from './NewChannel.module.css'
-
+import Password from './Password';
+import {useState} from "react"
 
 function NewChannel() {
-    function SelectOption(event) {
-        if (event.target.value === 'protected')
-            document.getElementById(classes.password).style.display = "flex";
-        else
-            document.getElementById(classes.password).style.display = "none";
-    }
+    const [choice, setChoice] = useState(false)
     return (<div className={classes.card}>
         <form className={classes.createForm}>
             <div className={classes.text}>Create new channel</div>
+            <h5>Channels are where your team communicate, They're best when to organized around topic </h5>
             <div className={classes.info}>
-                <label> Name: </label>
-                <input type="text" name="name" required />
-            </div>
-            <div className={classes.info}>
-                <label> Avatar: </label>
-                <input type="text" name="name" required />
+                <label> Channel name </label>
+                <input type="text" name="name" className={classes.inputs} required />
             </div>
             <div className={classes.info}>
-                <label> Status: </label>
-                <select name="status" onChange={SelectOption}>
-                    <option value="public">public</option>
-                    <option value="private">private</option>
-                    <option value="protected">protected</option>
-                </select>
+                <label> Description <span>(optional)</span> </label>
+                <textarea type="text" name="description" required />
             </div>
-            <div className={classes.info} id={classes.password}>
-                <label> Password: </label>
-                <input type="text" name="name" required />
+            <div className={classes.info}>
+                <label > Status </label>
+                <div className={classes.status}><input type="radio" name="status" onChange={()=>setChoice(false)}  /><label>public</label></div>
+                <div className={classes.status}><input type="radio" name="status"  onChange={()=>setChoice(false)}/><label>private</label></div>
+                <div className={classes.status}><input type="radio" name="status"  onChange={()=>setChoice(true)}/><label>protected</label></div>
             </div>
-            <button>Create</button>
+            {choice ?<Password />: null}
+            <div className={classes.info}>
+                <label> Add people <span>(You can skip this step for now) </span></label>
+                <input type="text" name="name" className={classes.inputs} required />
+
+            </div>
+            <div className={classes.buttons}>
+                <button id={classes.cancel}>Cancel</button>
+                <button id={classes.create}>Create</button>
+            </div>
         </form>
     </div>)
 }
